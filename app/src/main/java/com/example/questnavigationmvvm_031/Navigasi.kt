3e25c622fb95f6bc42e5405db81af4feb10c7f20
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-21 11:32:31 +0700

    menambahkan fun Navigasi

--- app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
+++ app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
@@ -1,6 +1,18 @@
 package com.example.questnavigationmvvm_031
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigationmvvm_031.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman {
     Form,
     Data
 }
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+)
