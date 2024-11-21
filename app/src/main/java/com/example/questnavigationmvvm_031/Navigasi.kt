0f5cf92650b255c015f21eae42d91c5900f2332e
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-21 11:34:15 +0700

    menambahkan isi padding dan nav host

--- app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
+++ app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
@@ -1,8 +1,10 @@
 package com.example.questnavigationmvvm_031
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.questnavigationmvvm_031.ui.viewmodel.MahasiswaViewModel
 
@@ -15,4 +17,14 @@ fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
-)
+){
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+
+        NavHost(
+            modifier = Modifier.padding(isipadding),
+            navController = navHost,
+            startDestination = Halaman.Form.name
+        )
+
+
