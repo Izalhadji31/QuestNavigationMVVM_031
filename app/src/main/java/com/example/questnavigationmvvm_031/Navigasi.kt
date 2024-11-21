e257c5fc69b2f9a2f374b9a095b02babb9ffc167
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-21 11:36:19 +0700

    menambahkan Form mahasiswa k dalam navigasi kt

--- app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
+++ app/src/main/java/com/example/questnavigationmvvm_031/Navigasi.kt
@@ -1,11 +1,17 @@
 package com.example.questnavigationmvvm_031
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigationmvvm_031.model.ListGender
+import com.example.questnavigationmvvm_031.ui.view.FormMahasiswaView
 import com.example.questnavigationmvvm_031.ui.viewmodel.MahasiswaViewModel
 
 enum class Halaman {
@@ -17,7 +23,7 @@ fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
-){
+) {
     Scaffold { isipadding ->
         val uiState by viewModel.dataModel.collectAsState()
 
@@ -25,6 +31,20 @@ fun Navigasi(
             modifier = Modifier.padding(isipadding),
             navController = navHost,
             startDestination = Halaman.Form.name
-        )
+        ) {
+            composable(route = Halaman.Form.name) {
+                val context = LocalContext.current
+                FormMahasiswaView(
+                    listGender = ListGender.listSex.map { genderId ->
+                        context.resources.getString(genderId)
+                    },
+                    onSubmitClick = { listData ->
+                        viewModel.SaveDataMhs(listData)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
 
-
+        }
+    }
+}
